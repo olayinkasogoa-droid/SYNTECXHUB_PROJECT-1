a9a9a8c5f10e813bd6304e4f23b295388b66bc74
Olayinka <olayinkasogoa@gmail.com>
2026-06-05 21:08:44 +0100

    Python.py

--- Python.py
+++ Python.py
@@ -1,40 +1,19 @@
 from scapy.all import ARP, Ether, srp
 
-def scan_network(target_ip):
-    # Create ARP request
-    arp = ARP(pdst=target_ip)
+target_ip = "192.168.1.0/24"
 
-    # Broadcast MAC address
-    ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+arp = ARP(pdst=target_ip)
+ether = Ether(dst="ff:ff:ff:ff:ff:ff")
 
-    packet = ether / arp
+packet = ether / arp
 
-    # Send packet and capture response
-    result = srp(packet, timeout=3, verbose=0)[0]
+result = srp(packet, timeout=3, verbose=0)[0]
 
-    # List of clients
-    clients = []
+clients = []
 
-    for sent, received in result:
-        clients.append({
-            "ip": received.psrc,
-            "mac": received.hwsrc
-        })
+for sent, received in result:
+    clients.append({'ip': received.psrc, 'mac': received.hwsrc})
 
-    return clients
-
-
-if __name__ == "__main__":
-    # Change this if your network is different
-    target = "192.168.1.0/24"
-
-    print(f"Scanning network: {target}\n")
-
-    devices = scan_network(target)
-
-    print("Active devices found:\n")
-    print("IP Address\t\tMAC Address")
-    print("-" * 40)
-
-    for device in devices:
-        print(f"{device['ip']}\t\t{device['mac']}")
+print("Devices found:")
+for client in clients:
+    print(client)
